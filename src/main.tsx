cef5bf60bfa30b17ea65dce1af8949f0ae214f50
Diego Bloise <diego-dev@outlook.com>
2024-11-11 23:59:25 -0300

    update: server

--- src/main.tsx
+++ src/main.tsx
@@ -1,57 +1,71 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App.tsx'
 
-const transactions = [
-  {
-    id: 5,
-    title: 'Internet',
-    amount: -200,
-    type: 'withdraw',
-    category: 'Casa',
-    createdAt: '2024-11-20'
-  },
-  {
-    id: 4,
-    title: 'Desenvolvimento de Website',
-    amount: 12000,
-    type: 'deposit',
-    category: 'Desenvolvimento',
-    createdAt: '2024-11-17'
-  },
-  {
-    id: 3,
-    title: 'Investimentos',
-    amount: 2900,
-    type: 'deposit',
-    category: 'Investimentos',
-    createdAt: '2024-11-08'
-  },
-  {
-    id: 2,
-    title: 'Restaurante',
-    amount: -300,
-    type: 'withdraw',
-    category: 'Alimentação',
-    createdAt: '2024-11-03'
-  },
-  {
-    id: 1,
-    title: 'Aluguel',
-    amount: -2000,
-    type: 'withdraw',
-    category: 'Casa',
-    createdAt: '2024-11-01'
-  }
-];
-
 createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 5,
+          title: 'Internet',
+          amount: -200,
+          type: 'withdraw',
+          category: 'Casa',
+          createdAt: new Date('2024-11-20 09:00:00')
+        },
+        {
+          id: 4,
+          title: 'Desenvolvimento de Website',
+          amount: 12000,
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          createdAt: new Date('2024-11-17 09:00:00')
+        },
+        {
+          id: 3,
+          title: 'Investimentos',
+          amount: 2900,
+          type: 'deposit',
+          category: 'Investimentos',
+          createdAt: new Date('2024-11-08 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Restaurante',
+          amount: -300,
+          type: 'withdraw',
+          category: 'Alimentação',
+          createdAt: new Date('2024-11-03 09:00:00')
+        },
+        {
+          id: 1,
+          title: 'Aluguel',
+          amount: -2000,
+          type: 'withdraw',
+          category: 'Casa',
+          createdAt: new Date('2024-11-01 09:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return transactions
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   },
 })
